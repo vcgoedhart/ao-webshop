f3a8fd2203e13942c536561eca04a7929aa59642
Vincent Goedhart <vcgoedhart@hotmail.com>
2020-05-20 00:33:09 +0200

    Added category migration

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+
+class CategoryController extends Controller
+{    
+    // /**
+    //  * Create a new controller instance.
+    //  *
+    //  * @return void
+    //  */
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $categories = DB::table('categories')->get();
+
+        return view('category', ['categories' => $categories]);
+    }
+}
---
+++ resources/views/category.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app') @section('content')
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>Categories</h1>
+            <hr>
+            
+            <div class="grid-container">
+                @foreach ($categories as $category)
+                    <div class="mb-5 card" style="width: 18rem;">
+                        <img class="card-img-top" src="{{ asset("img/card-category-$category->name.png") }}" alt="category image">
+                        <div class="card-body text-center">
+                            <h2 class="card-title">{{ $category->name }}</h2>
+                            <p class="card-text">{{ $category->description }}</p>
+                            <a href="product/{{ $category->id }}" class="btn btn-primary">Meer...</a>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
