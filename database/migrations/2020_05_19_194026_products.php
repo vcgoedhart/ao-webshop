57373cc1453ecf607960e7382cb7de297e774c41
Vincent Goedhart <vcgoedhart@hotmail.com>
2020-05-27 14:38:08 +0200

    Created > varchar max for the description column

--- database/migrations/2020_05_19_194026_products.php
+++ database/migrations/2020_05_19_194026_products.php
@@ -16,7 +16,7 @@ class Products extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('description');
+            $table->string('description', 2000);
             $table->string('price');
             $table->integer('category_id')->unsigned();
             $table->timestamps();
