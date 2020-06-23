99a6519a052c8ae435f3ecf33338808512e8f711
Vincent Goedhart <vcgoedhart@hotmail.com>
2020-06-24 01:07:40 +0200

    Added order migration

---
+++ database/migrations/2020_06_04_131917_orders.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Orders extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('cart', 5000);
+            $table->integer('user_id')->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
